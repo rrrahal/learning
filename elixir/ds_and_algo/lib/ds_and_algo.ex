06488183798712f61ae32321efd36ff08bae56e5
Rafael Rahal <rrrahal@gmail.com>
2025-03-11 20:55:54 +0000

    [elixir] chore: basic project for ds and algo

---
+++ elixir/ds_and_algo/lib/ds_and_algo.ex
@@ -0,0 +1,18 @@
+defmodule DsAndAlgo do
+  @moduledoc """
+  Documentation for `DsAndAlgo`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> DsAndAlgo.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ elixir/ds_and_algo/test/ds_and_algo_test.exs
@@ -0,0 +1,8 @@
+defmodule DsAndAlgoTest do
+  use ExUnit.Case
+  doctest DsAndAlgo
+
+  test "greets the world" do
+    assert DsAndAlgo.hello() == :world
+  end
+end
